17d6e971270b84e3fbfb8bcc6324e3d3acb326c4
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-11-19 11:39:07 +0000

    My first portfolio, all done!

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,10 +21,83 @@ import resume from "../resume.pdf";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "https://www.istockphoto.com/en/photo/3d-rendered-classic-sculpture-metaverse-avatar-with-network-of-low-poly-glowing-gm1401980646-455043990?phrase=ai";
+import image from "/**
+* Home component
+*
+* The section at the top of the page to display image of your
+* choice, name and title that describes your career focus.
+*/
+
+import React from "react";
+import arrowSvg from "../images/down-arrow.svg";
+import PropTypes from "prop-types";
+import resume from "../resume.pdf";
+
+/**
+* Home background image
+*
+* Below is a sample image. Upload the image of your choice into the "images"
+* directory and import here for use. Then, set imageAltText to string that
+* represents what you see in that image.
+*
+*
+* Need an image? Check out https://unsplash.com to download a photo you
+* freely use on your site.
+*/
+import image from "src/images/socials/quantum-physics-4550602.jpg";
 
 const imageAltText =
-  "an ai generated fantasy image representing the artificial intelligence";
+ "an artificial intelligence image";
+const Home = ({ name, title }) => {
+ /**
+  * Downloads the resume file stored in the path .
+  * @function
+  * @name download
+  * @returns {void}
+  * @description Downloads the resume file stored in the path when the Download Resume button is clicked. Uses the HTML5 download attribute to download the file.
+  */
+ const handleDownload = () => {
+   const link = document.createElement("a");
+   link.href = resume;
+   link.download = "resume.pdf";
+   link.click();
+ };
+
+ return (
+   <section id="home" className="min-height">
+     <img className="background" src={image} alt="" />
+     <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+       <h1>{name}</h1>
+       <h2>{title}</h2>
+       {/* added a button on the front page to download the entire website as a pdf (credits: Alishba)*/}
+       <button onClick={handleDownload} className="download-btn">
+         Download Resume
+       </button>
+     </div>
+     <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
+       <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+     </div>
+   </section>
+ );
+};
+
+Home.defaultProps = {
+ name: "",
+ title: "",
+ resume: "",
+};
+
+Home.propTypes = {
+ name: PropTypes.string.isRequired,
+ title: PropTypes.string.isRequired,
+ resume: PropTypes.string.isRequired,
+};
+
+export default Home;
+";
+
+const imageAltText =
+  "an artificial intelligence image ";
 const Home = ({ name, title }) => {
   /**
    * Downloads the resume file stored in the path .
