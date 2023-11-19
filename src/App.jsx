56d06434f3057763d11074d128f262b9b7f5bf11
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-11-19 10:06:19 +0000

    My first portfolio,all done!

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,14 @@ import "./styles.css";
  */
 const siteProps = {
   name: "Ghulam Murtaza Kazi",
-  title: "Web Designer & Content Creator",
+  title: "Developer",
   email: "ghulammurtazaqazi1@gmail.com",
   gitHub: "https://github.com/GhulamMurtazaKazi",
-  instagram: "microsoft",
+  instagram: "https://instagram.com/ghulammurtazakazi?igshid=M2RkZGJiMzhjOQ==",
   linkedIn: "https://www.linkedin.com/in/ghulam-murtaza-qazi-815804290/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BlQTHCeZKRc%2BBe%2FyrD64oGA%3D%3D",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://x.com/GhulamM17078721?t=1_YU0c2KnnwUKOwaTO1S5w&s=09",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
